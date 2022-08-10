19c954842b53d8fc2ee103fd1e3b8710fcd28dda
Pankaj1811 <110079643+Pankaj1811@users.noreply.github.com>
2022-08-10 17:52:02 +0530

    Update yosys_run.sh

--- yosys_run.sh
+++ yosys_run.sh
@@ -11,4 +11,6 @@ abc -liberty /home/pankaj/iiitb_bidicntr/lib/sky130_fd_sc_hd__tt_025C_1v80.lib
 clean
 flatten
 # write synthesized design
-write_verilog iiitb_bidicntr_synth.v
+write_verilog iiitb_bidicntr_synth.v 
+stat # to show stats
+show #for the schematic
